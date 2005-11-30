bcf172e957e9e24e3f8e0108903b9f59022492dc
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2005-11-30 23:10:05 +0000

    Still does not work, but a lot of useful stuff is added

--- src/java/se/idega/idegaweb/commune/childcare/business/ContractAndPlacementChangesExportWriter.java
+++ src/java/se/idega/idegaweb/commune/childcare/business/ContractAndPlacementChangesExportWriter.java
@@ -1,6 +1,7 @@
 package se.idega.idegaweb.commune.childcare.business;
 
 import java.io.InputStream;
+import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
@@ -11,6 +12,8 @@ import se.idega.idegaweb.commune.care.data.ChildCareContractHome;
 import com.idega.block.school.business.SchoolBusiness;
 import com.idega.block.school.data.SchoolClassMember;
 import com.idega.block.school.data.SchoolClassMemberHome;
+import com.idega.block.school.data.SchoolClassMemberLog;
+import com.idega.block.school.data.SchoolClassMemberLogHome;
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
 import com.idega.core.file.data.ICFile;
@@ -114,23 +117,47 @@ public class ContractAndPlacementChangesExportWriter {
 	//XXX: get first and last date of current month
 	
 	private Date getFirstDateOfCurrentMonth() {		
-	    Calendar cal = new GregorianCalendar();	  
+	    Calendar cal = roundCalendarToDate(new GregorianCalendar());
 	        
 	    int firstDay = cal.getActualMinimum(Calendar.DAY_OF_MONTH); // It will be 1	    
 	    cal.set(Calendar.DAY_OF_MONTH, firstDay);
 	    
 	    return new Date(cal.getTimeInMillis());
-	}
-	
+	}	
+
 	private Date getLastDateOfCurrentMonth() {		
-	    Calendar cal = new GregorianCalendar();	  
+	    Calendar cal = roundCalendarToDate(new GregorianCalendar());	  
 	        
 	    int days = cal.getActualMaximum(Calendar.DAY_OF_MONTH);	    
 	    cal.set(Calendar.DAY_OF_MONTH, days);
-	    
+
 	    return new Date(cal.getTimeInMillis());
-	}	
+	}
 	
+	private Calendar roundCalendarToDate(Calendar cal){
+	    cal.set(Calendar.HOUR, 0);
+	    cal.set(Calendar.MINUTE, 0);
+	    cal.set(Calendar.SECOND, 0);
+	    cal.set(Calendar.MILLISECOND, 0);
+	    return cal;
+	}	
+
+	private Date timestampToRoundedDate(Timestamp stamp) {
+		if (stamp == null) return null;
+		
+		Calendar cal = new GregorianCalendar();
+		cal.setTimeInMillis(stamp.getTime());
+		return roundCalendarToDate(cal).getTime();
+		
+	}
+	
+	private Date roundDate(Date date) {
+		if (date == null) return null;
+		
+		Calendar cal = new GregorianCalendar();
+		cal.setTimeInMillis(date.getTime());
+		return roundCalendarToDate(cal).getTime();
+	}
 	
 	private String nullToEmpty(String s) {
 		if(s == null) {
@@ -142,22 +169,100 @@ public class ContractAndPlacementChangesExportWriter {
 	}
 	
 	private void testing()  {
+		Date firstDateOfCurrentMonth = this.getFirstDateOfCurrentMonth();
+		Date lastDateOfCurrentMonth = this.getLastDateOfCurrentMonth();
+		
+		IWTimestamp stamp = new IWTimestamp(lastDateOfCurrentMonth);
+		String s = stamp.getDateString("yyyy-MM-dd");
+		
 		try {
 			//get all placements
-			Collection placements = getSchoolClassMemberHome().findAllByCategory(getSchoolBusiness().getCategoryChildcare());
-			
 			//retrieve the placements we need
 			//... and add them to the ubercolection
-			
-			
+			Iterator placementsIterator =  getSchoolClassMemberHome().findAllByCategory(getSchoolBusiness().getCategoryChildcare()).iterator();
+			for(; placementsIterator.hasNext();) {
+				SchoolClassMember placement = (SchoolClassMember) placementsIterator.next();
+				
+				//now, when will we take this placement?
+//				2.	IF it’s a new placement where sch_class_member.register_date >= 1st of current month 
+//					AND sch_class_member.register_date <= last date of current month
+				Date registerDate = timestampToRoundedDate(placement.getRegisterDate()); //java.sql.Timestamp; and only contains date
+				if (registerDate.compareTo(firstDateOfCurrentMonth) < 0 || registerDate.compareTo(lastDateOfCurrentMonth) > 0) {
+					continue;
+				}
+				
+//				3.	IF it’s a placement that ends (has removed_date set in sch_class_member) and 
+//					sch_class_member.removed date <= last date of current month
+				Date removedDate = timestampToRoundedDate(placement.getRemovedDate()); //java.sql.Timestamp; contains date and time
+				if (removedDate != null && (registerDate.compareTo(firstDateOfCurrentMonth) < 0 || registerDate.compareTo(lastDateOfCurrentMonth) > 0) ) {
+					continue;
+				}
+				
+				Iterator placementLogs = getSchoolClassMemberLogHome().findAllBySchoolClassMember(placement).iterator();
+				while (placementLogs.hasNext()){
+					SchoolClassMemberLog log = (SchoolClassMemberLog) placementLogs.next();
+					
+					Date startDate = log.getStartDate(); //java.util.Date, contains date only
+					Date endDate = log.getEndDate(); //java.util.Date, contains date only
+					
+					if (registerDate.equals(startDate)) {
+						//placement started
+						// add and continue
+					}
+					
+					if (removedDate != null && removedDate.equals(endDate)){
+						//placement ended
+//						 add and continue
+					}
+					
+
+//					4.	IF a group has been changed (new entry in sch_class_member_log with 
+//					sch_class_member_log.start date >= 1st of current month AND 
+//					sch_class_member_log.start date <= last date of current month)	
+					
+					if (startDate.compareTo(firstDateOfCurrentMonth)>= 0) {
+//						none of the above, so group was changed
+						//add 
+						
+					}			
+					
+				}
+				
+			}
 			
 			//get all contracts
-			Collection contracts = getChildCareContractHome().findChangedBetween(new java.sql.Date(1111), new java.sql.Date(1111));
 			//retrieve the contracts we need
 			//... and add them to the ubercollection
 			
+			//btw, table comm_childcare_archive = ChildCareContractBMPBean
+			Iterator contractsIterator = getChildCareContractHome().findChangedBetween(
+					new java.sql.Date(firstDateOfCurrentMonth.getTime()), new java.sql.Date(lastDateOfCurrentMonth.getTime())).iterator(); //TODO: check, if this date conversion is okay.
 			
 			
+			while (contractsIterator.hasNext()) {
+				ChildCareContract contract = (ChildCareContract) contractsIterator.next();
+				
+//				5.	IF a caretime has been changed (new entry in comm_childcare_archive with 
+//					comm_childcare_archive.start date >=1st of current month AND comm_childcare_archive.start date <= last date of current month)
+				Date validFromDate = contract.getValidFromDate(); //java.util.Date , only date
+				
+				if (validFromDate.compareTo(firstDateOfCurrentMonth) >= 0 || validFromDate.compareTo(lastDateOfCurrentMonth) <= 0) {
+					//add
+					SchoolClassMember member = contract.getSchoolClassMember(); 
+					member.getSchoolClass().getSchool().getExtraProviderId();
+					member.getSchoolClass().getGroupStringId();
+					member.getStudent().getPersonalID();
+					contract.getValidFromDate();
+					contract.getTerminatedDate();
+					contract.getCareTime(); // it's okay and actually returns care_time_string
+					member.getSchoolType().getTypeStringId();
+					
+				}
+				
+				
+			}
+
+						
 			
 			
 			//reshuffle the ubercollection so it is in correct order
@@ -176,6 +281,10 @@ public class ContractAndPlacementChangesExportWriter {
 	private SchoolClassMemberHome getSchoolClassMemberHome() throws IDOLookupException {
 		return (SchoolClassMemberHome) IDOLookup.getHome(SchoolClassMember.class);
 	}
+	
+	private SchoolClassMemberLogHome getSchoolClassMemberLogHome() throws IDOLookupException {
+		return (SchoolClassMemberLogHome) IDOLookup.getHome(SchoolClassMember.class);
+	}	
 
 	private SchoolBusiness getSchoolBusiness() throws IBOLookupException{
 		return (SchoolBusiness) IBOLookup.getServiceInstance(iwc, SchoolBusiness.class);   
@@ -185,6 +294,13 @@ public class ContractAndPlacementChangesExportWriter {
 		return (ChildCareContractHome) IDOLookup.getHome(ChildCareContract.class);
 	}
 	
-	
+	private class DataForExport implements Comparable {
+		int aaa;
+
+		public int compareTo(Object arg0) {  //must return > 0 if argument is smaller
+			// TODO Auto-generated method stub
+			return 0;
+		}
+	}
 	
 }
